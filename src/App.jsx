246342aca274ef9cc6bff58a6c7bec3a7ffe7032
Pranay Prasad <pranayprasad42@gmail.com>
2025-02-18 10:41:50 +0000

    structure admin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Component/Navbar'
+import Skill from './Component/Skill';
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Skill/>
+    </>
+  );
+}
+
+export default App;
